13881c36eea2602074a28703079b136da99bd6fc
dashinfall3 <christophersell3@gmail.com>
2013-02-14 14:05:53 -0600

    Update app/models/student.rb

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,39 @@
 require_relative '../../db/config'
+require 'date'
+
+class GoodnessValidator < ActiveModel::Validator
+  def validate(record)
+    if record.age < 5
+      record.errors[:base] << "This person is a toddler!"
+    end
+      
+  end
+end
 
 # implement your Student model here
+class Student < ActiveRecord::Base
+	REGEX = /\w*@\w*.[a-zA-Z]{2,5}/
+	validates_with GoodnessValidator
+	validates :email, :uniqueness => true
+	validates :email, :format => { :with => REGEX,
+					    			:message => "Only valid emails allowed" }
+	validate :phone_number_size
+
+	def phone_number_size
+		if self.phone.scan(/\d/).join('').length < 10
+			errors.add(:discount, "too few numbers")
+		end
+	end
+
+	def name
+		"#{self.first_name} #{self.last_name}"
+	end
+
+	def age
+		year_now = Date.today.strftime("%Y").to_i
+		year_birth = self.birthday.strftime("%Y").to_i
+		year_now - year_birth -1
+	end
+end
+
+
